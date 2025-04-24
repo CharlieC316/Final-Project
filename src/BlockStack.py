4d54f0d9f7bcb0fd6b35b073b6a4df8fa1e7780b
CharlieC316 <chcarnahan@gmail.com>
2025-04-24 17:32:09 -0500

    added a filler background image for where the blocks will be stacked

--- src/BlockStack.py
+++ src/BlockStack.py
@@ -9,5 +9,9 @@ SCREEN_WIDTH, SCREEN_HEIGHT = 300, 600
 BLOCK_SIZE = 30
 FPS = 60
 
+# Colors & Textures
+white = (255, 255, 255)
+red = (255, 0, 0)
+backgroundImage = pygame.image.load("BackgroundImage.jpg")
 
 pygame.quit()
